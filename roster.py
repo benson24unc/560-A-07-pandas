18527e39035d7a71b1d13093e506fe9044c5d0fc
benson24unc <benson24@ad.unc.edu>
2024-04-01 18:36:25 -0400

    0 header

--- roster.py
+++ roster.py
@@ -1,6 +1,7 @@
 #https://goheels.com/sports/mens-basketball/roster
+import pandas as pd
 
-roster = ['Lebo', 'Davis','Trimble']
 
-for player in roster:
-    print(player)
+roster = ['Lebo', 'Davis', 'Trimble']
+data = pd.DataFrame(roster)
+print(data)
