03f935d4eae0f861941ad9ac9d04534239f3209d
benson24unc <benson24@ad.unc.edu>
2024-05-09 21:08:00 -0400

    last name header

--- roster.py
+++ roster.py
@@ -1,8 +1,19 @@
 #https://goheels.com/sports/mens-basketball/roster
 import pandas as pd
 
+player_data = {
+    "Last Name": ["Davis", "Trimble", "Lebo"],
+    "First Name": ["RJ", "Seth", "Creighton"],
+    "Height": [72, 75, 73], 
+    "Weight": [180, 195, 180]  
+}
 
-last_names = ['Lebo', 'Davis', 'Trimble']
+data = pd.DataFrame(player_data)
 
-data = pd.DataFrame(last_names, columns=['Last Name'])
+
+data["BMI"] = (data["Weight"] / 2.205) / ((data["Height"] * 0.0254) ** 2)
+
+
 print(data)
+
+data.to_csv("bmi.csv")
